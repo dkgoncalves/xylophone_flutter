d63e5bc4eb06b682a2b5e6add7df064c4af07ce6
Derik <derik@oagro.com.br>
2020-11-03 17:53:43 -0300

    Creating playSoud and buildKey methods and finishing project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -7,10 +8,40 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.red, audioNumber: 1),
+              buildKey(color: Colors.orange, audioNumber: 2),
+              buildKey(color: Colors.yellow, audioNumber: 3),
+              buildKey(color: Colors.green, audioNumber: 4),
+              buildKey(color: Colors.teal, audioNumber: 5),
+              buildKey(color: Colors.blue, audioNumber: 6),
+              buildKey(color: Colors.purple, audioNumber: 7),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  void playSound({int audio}) {
+    final player = AudioCache();
+    player.play('note$audio.wav');
+  }
+
+  Expanded buildKey({Color color, int audioNumber}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: (){
+          playSound(audio: audioNumber);
+        },
+      ),
+    );
+  }
 }
+
+
